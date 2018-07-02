e5e23c1a097fe93b671ad8b37ed5a4a9f60f06b3
Broc Nickodemus <nickodeb@oregonstate.edu>
2018-07-01 20:34:13 -0700

    added touch sensor module

---
+++ osoyoo sensor kit/Touch_sensor_module/Touch_sensor_module.ino
@@ -0,0 +1,20 @@
+/* touch sensor test project
+ * project tutorial url: http://osoyoo.com/?p=501
+ */
+int digit_sensor = 12; // select the input pin for the potentiometer
+int ledPin = 3; // select the pin for the LED
+int digitValue ;  //  value from the digit input pin
+
+void setup () {
+  pinMode (ledPin, OUTPUT);
+  pinMode (digit_sensor, INPUT);
+}
+
+void loop () {
+  digitValue=digitalRead(digit_sensor);
+
+  if (digitValue==HIGH)
+    digitalWrite (ledPin, HIGH);
+  else 
+    digitalWrite (ledPin, LOW);
+}
